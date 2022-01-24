6d6c9c2b68b6377ecefa792f5a2539ad603c99ec
hohMiyazawa <>
2022-01-24 23:48:54 +0100

    more movegen

--- hengine.js
+++ hengine.js
@@ -85,10 +85,14 @@ null,
 let rawMoveGen = function(board){
 	let list = [];
 	board.pos.forEach((pos,index) => {
-		if(pos === 0){
+		if(
+			pos === 0
+			|| (board.isWhite && pos > 8)
+			|| (!board.isWhite && pos < 8)
+		){
 			return
 		}
-		else if(pos === 1 && board.isWhite){
+		else if(pos === 1){//white pawn
 			let front = board.pos[index + 8];
 			if(!front){
 				if(index < 16){//start square
@@ -114,9 +118,9 @@ let rawMoveGen = function(board){
 					queenPromotion.material.white[4]++;
 					rookPromotion.pos[index + 8] = 2;
 					rookPromotion.material.white[1]++;
-					bishopPromotion.pos[index + 8] = 3;
+					bishopPromotion.pos[index + 8] = 4;
 					bishopPromotion.material.white[3]++;
-					knightPromotion.pos[index + 8] = 2;
+					knightPromotion.pos[index + 8] = 3;
 					knightPromotion.material.white[2]++;
 					list.push(queenPromotion);
 					list.push(rookPromotion);
@@ -144,9 +148,9 @@ let rawMoveGen = function(board){
 						queenPromotion.material.white[4]++;
 						rookPromotion.pos[index + 8 - 1] = 2;
 						rookPromotion.material.white[1]++;
-						bishopPromotion.pos[index + 8 - 1] = 3;
+						bishopPromotion.pos[index + 8 - 1] = 4;
 						bishopPromotion.material.white[3]++;
-						knightPromotion.pos[index + 8 - 1] = 2;
+						knightPromotion.pos[index + 8 - 1] = 3;
 						knightPromotion.material.white[2]++;
 						list.push(queenPromotion);
 						list.push(rookPromotion);
@@ -175,9 +179,9 @@ let rawMoveGen = function(board){
 						queenPromotion.material.white[4]++;
 						rookPromotion.pos[index + 8 + 1] = 2;
 						rookPromotion.material.white[1]++;
-						bishopPromotion.pos[index + 8 + 1] = 3;
+						bishopPromotion.pos[index + 8 + 1] = 4;
 						bishopPromotion.material.white[3]++;
-						knightPromotion.pos[index + 8 + 1] = 2;
+						knightPromotion.pos[index + 8 + 1] = 3;
 						knightPromotion.material.white[2]++;
 						list.push(queenPromotion);
 						list.push(rookPromotion);
@@ -191,16 +195,146 @@ let rawMoveGen = function(board){
 			}
 			if(board.enPassantFile && index < 16){
 				if(index - 8 === board.enPassantFile){
+					let move = copyNext(board);
+					move.pos[index + 8 - 1] = 1;
+					move.pos[index - 1] = 0;
+					move.pos[index] = 0;
+					move.moveClock = 0;
+					move.material.black[0]--;
+					list.push(move)
 				}
 				else if(index - 6 === board.enPassantFile){
+					let move = copyNext(board);
+					move.pos[index + 8 + 1] = 1;
+					move.pos[index + 1] = 0;
+					move.pos[index] = 0;
+					move.moveClock = 0;
+					move.material.black[0]--;
+					list.push(move)
 				}
 			}
 		}
-		else if(pos === 9 && !board.isWhite){
-		}
-		else if(pos === 6 && board.isWhite){
-		}
-		else if(pos === 14 && !board.isWhite){
+		else if(pos === 9){//black pawn
+			let front = board.pos[index - 8];
+			if(!front){
+				if(index > 47){//start square
+					if(!board.pos[index - 16]){
+						let move = copyNext(board);
+						move.pos[index - 16] = 9;
+						move.pos[index] = 0;
+						move.moveClock = 0;
+						move.enPassantFile = (index % 8) + 1;
+						list.push(move)
+					}
+				}
+				let move = copyNext(board);
+				move.pos[index - 8] = 9;
+				move.pos[index] = 0;
+				move.moveClock = 0;
+				if(index < 16){//promotions
+					let queenPromotion = copyBoard(move);
+					let rookPromotion = copyBoard(move);
+					let bishopPromotion = copyBoard(move);
+					let knightPromotion = copyBoard(move);
+					queenPromotion.pos[index - 8] = 13;
+					queenPromotion.material.black[4]++;
+					rookPromotion.pos[index - 8] = 10;
+					rookPromotion.material.black[1]++;
+					bishopPromotion.pos[index - 8] = 12;
+					bishopPromotion.material.black[3]++;
+					knightPromotion.pos[index - 8] = 11;
+					knightPromotion.material.black[2]++;
+					list.push(queenPromotion);
+					list.push(rookPromotion);
+					list.push(bishopPromotion);
+					list.push(knightPromotion)
+				}
+				else{
+					list.push(move)
+				}
+			}
+			if(index % 8 !== 0){
+				let left = board.pos[index - 8 - 1];
+				if(left < 8){
+					let move = copyNext(board);
+					move.pos[index - 8 - 1] = 1;
+					move.pos[index] = 0;
+					move.moveClock = 0;
+					move.material.black[code_to_materialIndex[left]]--;
+					if(index < 16){//promotions
+						let queenPromotion = copyBoard(move);
+						let rookPromotion = copyBoard(move);
+						let bishopPromotion = copyBoard(move);
+						let knightPromotion = copyBoard(move);
+						queenPromotion.pos[index - 9] = 13;
+						queenPromotion.material.black[4]++;
+						rookPromotion.pos[index - 9] = 10;
+						rookPromotion.material.black[1]++;
+						bishopPromotion.pos[index - 9] = 12;
+						bishopPromotion.material.black[3]++;
+						knightPromotion.pos[index - 9] = 11;
+						knightPromotion.material.black[2]++;
+						list.push(queenPromotion);
+						list.push(rookPromotion);
+						list.push(bishopPromotion);
+						list.push(knightPromotion)
+					}
+					else{
+						list.push(move)
+					}
+				}
+			}
+			if(index % 8 !== 7){
+				let right = board.pos[index - 8 + 1];
+				if(right < 8){
+					let move = copyNext(board);
+					move.pos[index + 8 + 1] = 1;
+					move.pos[index] = 0;
+					move.moveClock = 0;
+					move.material.black[code_to_materialIndex[right]]--;
+					if(index < 16){//promotions
+						let queenPromotion = copyBoard(move);
+						let rookPromotion = copyBoard(move);
+						let bishopPromotion = copyBoard(move);
+						let knightPromotion = copyBoard(move);
+						queenPromotion.pos[index - 7] = 13;
+						queenPromotion.material.black[4]++;
+						rookPromotion.pos[index - 7] = 10;
+						rookPromotion.material.black[1]++;
+						bishopPromotion.pos[index - 7] = 12;
+						bishopPromotion.material.black[3]++;
+						knightPromotion.pos[index - 7] = 11;
+						knightPromotion.material.black[2]++;
+						list.push(queenPromotion);
+						list.push(rookPromotion);
+						list.push(bishopPromotion);
+						list.push(knightPromotion)
+					}
+					else{
+						list.push(move)
+					}
+				}
+			}
+			if(board.enPassantFile && index > 47){
+				if(((index % 8) + 2) === board.enPassantFile){
+					let move = copyNext(board);
+					move.pos[index - 8 + 1] = 9;
+					move.pos[index + 1] = 0;
+					move.pos[index] = 0;
+					move.moveClock = 0;
+					move.material.white[0]--;
+					list.push(move)
+				}
+				else if((index % 8) === board.enPassantFile){
+					let move = copyNext(board);
+					move.pos[index - 8 - 1] = 9;
+					move.pos[index - 1] = 0;
+					move.pos[index] = 0;
+					move.moveClock = 0;
+					move.material.white[0]--;
+					list.push(move)
+				}
+			}
 		}
 		else if(pos === 3 || pos === 11){
 			let indices;
@@ -236,131 +370,130 @@ let rawMoveGen = function(board){
 			})
 		}
 		else{
-			for(let i = 1;i < 8;i++){
-				if(!(index + i % 8)){
-					break
-				}
-				let piece = board.pos[index + i];
-				if(!piece){
-					let move = copyNext(board);
-					move.pos[index + i] = pos;
-					move.pos[index] = 0;
-					move.moveClock++;
-					if(board.isWhite){
-						if(index === 0){
-							move.white.ooo = false
-						}
-						else if(index === 7){
-							move.white.oo = false
-						}
-					}
-					else{
-						if(index === 57){
-							move.black.ooo = false
-						}
-						else if(index === 63){
-							move.black.oo = false
-						}
-					}
-					list.push(move)
-				}
-				else{
-					if((piece < 8 && board.isWhite) || (piece > 8 && !board.isWhite)){
+			let offsets = [];
+			if(pos === 6 || pos === 14 || pos === 2 || pos === 10 || pos === 5 || pos === 13){
+				offsets.push([0,-1],[-1,0],[1,0],[0,1])
+			}
+			if(pos === 6 || pos === 14 || pos === 3 || pos === 11 || pos === 5 || pos === 13){
+				offsets.push([-1,-1],[1,-1],[-1,1],[1,1])
+			}
+			offsets.forEach(pair => {
+				for(let i = 1;i < 8;i++){
+					let target = index + i*pair[0] + i*pair[1]*8;
+					if(
+						((index % 8) + i*pair[0] < 0)
+						|| ((index % 8) + i*pair[0] > 7)
+						|| (target < 0)
+						|| (target > 63)
+						|| (i > 1 && (pos === 6 || pos === 14))
+					){
 						break
 					}
-					let material = code_to_materialIndex[piece];
-					let move = copyNext(board);
-					move.pos[index + i] = pos;
-					move.pos[index] = 0;
-					move.moveClock = 0;
-					if(board.isWhite){
-						if(index === 0){
-							move.white.ooo = false
+					let piece = board.pos[target];
+					if(!piece){//move
+						let move = copyNext(board);
+						move.pos[target] = pos;
+						move.pos[index] = 0;
+						move.moveClock++;
+						if(pos === 6){
+							move.kingPos.white = target;
+							move.castle.white.ooo = false;
+							move.castle.white.oo = false
+						}
+						else if(pos === 14){
+							move.kingPos.black = target;
+							move.castle.black.ooo = false;
+							move.castle.black.oo = false
+						}
+						else if(index === 0){
+							move.castle.white.ooo = false
 						}
 						else if(index === 7){
-							move.white.oo = false
+							move.castle.white.oo = false
 						}
-						move.material.black[material]--
-					}
-					else{
-						if(index === 57){
-							move.black.ooo = false
+						else if(index === 57){
+							move.castle.black.ooo = false
 						}
 						else if(index === 63){
-							move.black.oo = false
+							move.castle.black.oo = false
 						}
-						move.material.white[material]--
+						list.push(move)
 					}
-					list.push(move);
-					break
-				}
-			}
-			for(let i = 1;i < 8;i++){
-				if(!(index - i + 1 % 8)){
-					break
-				}
-				let piece = board.pos[index - i];
-				if(!piece){
-					let move = copyNext(board);
-					move.pos[index - i] = pos;
-					move.pos[index] = 0;
-					move.moveClock++;
-					if(board.isWhite){
-						if(pos === 0){
-							move.white.ooo = false
+					else{//capture
+						if((piece < 8 && board.isWhite) || (piece > 8 && !board.isWhite)){
+							break
 						}
-						else if(pos === 7){
-							move.white.oo = false
+						let material = code_to_materialIndex[piece];
+						let move = copyNext(board);
+						move.pos[target] = pos;
+						move.pos[index] = 0;
+						move.oveClock = 0;
+						if(board.isWhite){
+							move.material.black[material]--
 						}
-					}
-					else{
-						if(pos === 57){
-							move.black.ooo = false
+						else{
+							move.material.white[material]--
 						}
-						else if(pos === 63){
-							move.black.oo = false
+						if(pos === 6){
+							move.kingPos.white = target;
+							move.castle.white.ooo = false;
+							move.castle.white.oo = false
 						}
-					}
-					list.push(move)
-				}
-				else{
-					if((piece < 8 && board.isWhite) || (piece > 8 && !board.isWhite)){
+						else if(pos === 14){
+							move.kingPos.black = target;
+							move.castle.black.ooo = false;
+							move.castle.black.oo = false
+						}
+						else if(index === 0){
+							move.castle.white.ooo = false
+						}
+						else if(index === 7){
+							move.castle.white.oo = false
+						}
+						else if(index === 57){
+							move.castle.black.ooo = false
+						}
+						else if(index === 63){
+							move.castle.black.oo = false
+						}
+						list.push(move);
 						break
 					}
-					let material = code_to_materialIndex[piece];
-					let move = copyNext(board);
-					move.pos[index - i] = pos;
-					move.pos[index] = 0;
-					move.moveClock = 0;
-					if(board.isWhite){
-						if(pos === 0){
-							move.white.ooo = false
-						}
-						else if(pos === 7){
-							move.white.oo = false
-						}
-						move.material.black[material]--
-					}
-					else{
-						if(pos === 57){
-							move.black.ooo = false
-						}
-						else if(pos === 63){
-							move.black.oo = false
-						}
-						move.material.white[material]--
-					}
-					list.push(move);
-					break
 				}
-			}
+			})
 		}
-	})
+	});
 	return list
 }
 
 let isCheck = function(board,isWhite){
 	if(isWhite){
+		let index = board.kingPos.white;
+		let indices;
+		if(index % 8 === 0){
+			indices = [-15,-6,10,17]
+		}
+		else if(index % 8 === 1){
+			indices = [-17,-15,-6,10,15,17]
+		}
+		else if(index % 8 === 6){
+			indices = [-17,-15,-10,6,15,17]
+		}
+		else if(index % 8 === 7){
+			indices = [-17,-10,6,15]
+		}
+		else{
+			indices = [-17,-15,-10,-6,6,10,15,17]
+		}
+		indices.forEach(ind => {
+			let target = ind + index;
+			if(target >= 0 && target < 64){
+				let piece = board.pos[target];
+				if(piece === 11){
+					return true
+				}
+			}
+		})
 	}
 	else{
 	}
--- hohChess2.js
+++ hohChess2.js
@@ -1,19 +1,49 @@
-function hohChess(DOMlocation,options){
-	let board = document.createElement("div");
-	board.classList.add("hohBoard");
-	DOMlocation.appendChild(board);
-	let backBoard = document.createElement("img");
-	backBoard.classList.add("hohBackBoard");
-	backBoard.src = "board.png";
-	backBoard.width = 512;
-	backBoard.height = 512;
-	board.appendChild(backBoard);
+let hohChess = {
+	board: null,
+	options: null,
+	init: function(DOMlocation,options){
+		let board = document.createElement("div");
+		board.classList.add("hohBoard");
+		DOMlocation.appendChild(board);
+		let backBoard = document.createElement("img");
+		backBoard.classList.add("hohBackBoard");
+		backBoard.src = "board.png";
+		backBoard.width = 512;
+		backBoard.height = 512;
+		board.appendChild(backBoard);
 
-	let pieceContainer = document.createElement("div");
-	pieceContainer.classList.add("hohPieceContainer");
-	board.appendChild(pieceContainer);
+		hohChess.pieceContainer = document.createElement("div");
+		hohChess.pieceContainer.classList.add("hohPieceContainer");
+		board.appendChild(hohChess.pieceContainer);
 
-	let squareContainer = document.createElement("div");
-	squareContainer.classList.add("hohSquareContainer");
-	board.appendChild(squareContainer);
+		let squareContainer = document.createElement("div");
+		squareContainer.classList.add("hohSquareContainer");
+		board.appendChild(squareContainer);
+		hohChess.board = initialBoard();
+		hohChess.options = options;
+
+		hohChess.render(hohChess.board)
+	},
+	render: function(board){
+		const pieceMap = ["","Pw","Rw","Nw","Bw","Qw","Kw","","","Pb","Rb","Nb","Bb","Qb","Kb"];
+		Array.from(hohChess.pieceContainer.children).forEach(child => child.remove());
+		board.pos.forEach((piece,index) => {
+			if(piece){
+				let ele = document.createElement("img");
+				ele.classList.add("hohPiece");
+				ele.width = 64;
+				ele.height = 64;
+				if(hohChess.options.flip){
+					ele.style.left = 448 - (index % 8) * 64 + "px";
+					ele.style.top  = Math.floor(index / 8) * 64 + "px"
+				}
+				else{
+					ele.style.left = (index % 8) * 64 + "px";
+					ele.style.top  = 448 - Math.floor(index / 8) * 64 + "px"
+				}
+				ele.src = pieceMap[piece] + ".png";
+				hohChess.pieceContainer.appendChild(ele);
+			}
+		})
+	}
 }
